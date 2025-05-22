d757eb65f106618e2bcdc8e94f24c15aa142af8a
mccamo51 <mccamo51@gmail.com>
2025-05-22 16:12:40 +0000

    added a while loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,3 +3,11 @@ const arrayList = ["C is fun", "Python is cool", "JavaScript is amazing"]
 for(let x = 0; x < arrayList.length; x++){
     console.log(arrayList[x])
 }
+
+
+
+let x = 3;
+while (x < arrayList.length) {
+    console.log(arrayList[x]);
+    x++;
+}
